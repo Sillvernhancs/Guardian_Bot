4a5d0c889616669b117fbca8f22be898bc184a1b
Silver Nhan <76749421+Sillvernhancs@users.noreply.github.com>
2021-09-30 09:04:14 -0500

    update !checkAll from message type to embed.
    title

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { IntegrationExpireBehavior } from 'discord-api-types';
-import DiscordJS, { Intents } from 'discord.js'
+import DiscordJS, { Channel, Emoji, Intents, MessageEmbed } from 'discord.js'
 import dotenv from 'dotenv'
 const translate = require('@iamtraction/google-translate');
 const { Client, Message } = require('discord.js');
@@ -9,6 +9,7 @@ dotenv.config()
 const fs = require('fs');
 const readline = require('readline');
 var price_pool = new Map();
+var pardonCount = new Map();
 let OP = ["IamSilver"];
 
 const client = new DiscordJS.Client({
@@ -17,6 +18,29 @@ const client = new DiscordJS.Client({
         Intents.FLAGS.GUILD_MESSAGES
     ]
 })
+//------------------------------------------------------------------------------------------
+// return a string of user names/debt/pardons
+function listUsers() {
+    var line = "";
+    for (var entry of price_pool.entries()) {
+        line += entry[0] + "\n";
+    }
+    return line;
+}
+function listDebts() {
+    var line = "";
+    for (var entry of price_pool.entries()) {
+        line += entry[1] + "\n";
+    }
+    return line;
+}
+function listPardons() {
+    var line = "";
+    for (var entry of pardonCount.entries()) {
+        line += entry[1] + "\n";
+    }
+    return line;
+}
 //------------------------------------------------------------------------------------------
 // translate function
 function toEng(s : any, message : any) {
@@ -35,8 +59,10 @@ function toEng(s : any, message : any) {
 function save() {
     // save to the file pool.txt
     var line = "";
+    // sort the price pool and save it to the file.
+    price_pool = new Map([...price_pool.entries()].sort((b, a) => b[1] - a[1]));
     for (var entry of price_pool.entries()) {
-        line += entry[0] + " " + entry[1] + " \n";
+        line += entry[0] + " " + entry[1] + " " + pardonCount.get(entry[0]) + " \n";
     }
     // console.log(line);
     fs.writeFile('pool.txt', line, (err : any) => {
@@ -44,13 +70,16 @@ function save() {
             console.error(err)
             return
     }})
+
+    // console.log(price_pool)
+    // console.log(pardonCount)
 }
 //------------------------------------------------------------------------------------------
 // "fine function"
 function DETECT(s : any, message : any) {
     // regex made it slow but it's shorter and easier to read so...
     const regex_mom = /muu*m|moo*m|maa*mm*a*|maa*dd*aa*|m00*m|moo*ther|muu*ther|m00*ther/s;
-    const regex_your = /yoo*uu*r|uu*r|yee*3*r|y00*uu*r|thyy*|thoo*uu*|yuu*h/s;
+    const regex_your = /yoo*uu*r|uu*r|yee*3*r|y00*uu*r|thyy*|thoo*uu*|yuu*h|y33*r/s;
     if(regex_mom.test(s) && regex_your.test(s)) {
         message.reply({
             content: "$1"
@@ -64,9 +93,33 @@ function DETECT(s : any, message : any) {
             // create new user in the pool
             price_pool.set(nameNoSpace, 1);
         }
-        // save it to the pool.txt
-        save();
     }
+    //-----------------------------------------------------////
+    // my mom joke
+    else if (regex_mom.test(s) && s.includes("my")) {
+        // get rid of space in user names.
+        var nameNoSpace = message.author.username.replace(/ /g,"_");
+        // if the name already exist
+        if(price_pool.has(nameNoSpace)) {
+            if(price_pool.get(nameNoSpace) > 0 && pardonCount.get(nameNoSpace) > 0) {
+                message.reply({
+                    content: "1$ less to you"
+                })
+                price_pool.set(nameNoSpace, price_pool.get(nameNoSpace) - 1);
+                pardonCount.set(nameNoSpace, pardonCount.get(nameNoSpace) - 1);
+            } else {
+                message.reply({
+                    content: "nice"
+                })
+            }
+        } else {
+            message.reply({
+                content: "nice"
+            })
+        }
+    }
+    // save it to the pool.txt
+    save();
 }
 //------------------------------------------------------------------------------------------
 // boot up sequence
@@ -83,7 +136,10 @@ client.on("ready", () => {
         console.log(line)
         var data = line.split(" ");
         price_pool.set(data[0], parseInt(data[1], 10));
+        pardonCount.set(data[0], parseInt(data[2], 10));
     });
+    console.log(price_pool)
+    // console.log(pardonCount)
     console.log("\\------------------------------------------")
     console.log("syncing OP list...")
     //-----------------------// read file for DEV LIST
@@ -111,7 +167,8 @@ client.on("messageCreate", async (message) => {
     if(message.content === "!check") {
         if(price_pool.has(message.author.username)) {
             message.reply({
-                content: "You owe the server: $" + price_pool.get(message.author.username)
+                content: "You owe the server: $" + price_pool.get(message.author.username) 
+                        + "\nYour pardons: "+ pardonCount.get(message.author.username)
             })
         } else {
             message.reply({
@@ -123,16 +180,19 @@ client.on("messageCreate", async (message) => {
     }
     //----------------------------------// check everyone's debt
     if(message.content === "!checkAll") {
-        var line = "";
-        for (var entry of price_pool.entries()) {
-            line += entry[0] + "\t: $" + entry[1] + "\n";
-        }
-        message.reply({
-            content: line
-        })
+        var embed_ = new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('---The Yer Mum Mux pool---')
+        .addFields(
+            { name: 'Muxies', value: listUsers(), inline: true  },
+            { name: 'Yer Mum Jokes Made', value: listDebts(), inline: true },
+            { name: 'Pardons', value: listPardons(), inline: true },
+        ).setFooter("\u3000".repeat(10/*any big number works too*/)+"|");
+        message.channel.send({embeds:[embed_]});
     }
     //----------------------------------// dev mode increase debt
-    if((message.content.includes("$1 to") || message.content.includes("1$ to")) && message.author.username != "The guardian") {
+    
+    if((message.content.startsWith("$1 to") || message.content.startsWith("1$ to")) && message.author.username != "The guardian") {
         //---> if an OP let them, if not be sassy.
         if(OP.includes(message.author.username)) {
             var user_ = message.content.substr(6);
